413d2678d3a7069536e8b72c8da49a26eef2ede9
Olivier Sels <olivier.sels@gmail.com>
2017-01-17 16:56:28 +0100

    Adjust hacking for our latest kappa develop

--- zappa/util.py
+++ zappa/util.py
@@ -215,10 +215,9 @@ def get_event_source(event_source, lambda_arn, target_function, boto_session, dr
     # hopefully we can remove at least some of this soon.
     if svc == 's3':
         split_arn = lambda_arn.split(':')
-        arn_front = ':'.join(split_arn[:-1])
         arn_back = split_arn[-1]
         ctx.environment = arn_back
-        funk.arn = arn_front
+        funk.arn = lambda_arn
         funk.name = ':'.join([arn_back, target_function])
     else:
         funk.arn = lambda_arn
